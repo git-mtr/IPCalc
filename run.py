1b21fa213af8096624517647712ff1a85e4a6b74
Git-Mtr <git-mtr@posteo.de>
2023-01-23 18:58:20 +0100

    mod: run.py - argv[3]

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import sys
 def main():
     # extract your arg here
     print('Extracted arg is ==> %s' % sys.argv[2])
+    print('Extracted arg is ==> %s' % sys.argv[3])
     pytest.main([sys.argv[1]])
 
 if __name__ == '__main__':
