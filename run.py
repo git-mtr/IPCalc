7f540b3306a929e5092c3648b0b94515d043c31d
Git-Mtr <git-mtr@posteo.de>
2023-01-23 18:51:39 +0100

    add: run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+# run.py
+import pytest
+import sys
+
+def main():
+    # extract your arg here
+    print('Extracted arg is ==> %s' % sys.argv[2])
+    pytest.main([sys.argv[1]])
+
+if __name__ == '__main__':
+    main()
